aea3ec7814348f136d164f7b5fc03e715a96e904
Bryce Boe <bbzbryce@gmail.com>
2021-02-11 14:08:43 -0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |x|
+  HighScore.create!(game: x, score: x + 11)
+end
